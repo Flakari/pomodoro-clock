42cbfb1c49b75fd762dd4089ca1eec1fe4f68072
Nate Dimock <natedimock@yahoo.com>
2018-01-25 21:41:51 -0600

    Added break timer increment functionality, added auto pomodoro session timing functionality

--- scripts/script.js
+++ scripts/script.js
@@ -2,22 +2,32 @@ let test = document.querySelector('#test');
 let testButton = document.querySelector('#test-button');
 let resetButton = document.querySelector('#reset');
 let running = false;
+
 let work = document.querySelector('#work');
 let workSession = work.querySelector('p');
 let workButtons = work.querySelectorAll('button');
+
+let br = document.querySelector('#break');
+let brSession = br.querySelector('p');
+let brButtons = br.querySelectorAll('button');
+
+let working = true;
+let workButton = true;
+let breakButton = false;
 
 let workTimer = 25;
 let breakTimer = 5;
 let longBreak = 15;
 
+let sessionAmt = 0;
 let minutes = 25
 let seconds = 0;
 let currentTime;
 let timer;
 let pause;
 
-minutesDisplay = minutes;
-secondsDisplay = '00';
+let minutesDisplay = minutes;
+let secondsDisplay = '00';
 
 test.textContent = minutesDisplay + ':' + secondsDisplay; 
 
@@ -39,8 +49,30 @@ function startTimer() {
         seconds--;
     }
     timer = setInterval(() => {
-        if (minutes == 0 && seconds == 0) {
-            clearInterval(timer);
+        if (minutes == 0 && seconds < 0) {
+            if (working) {
+                if (sessionAmt == 3) {
+                    working = false
+                    minutes = longBreak;
+                    seconds = 0;
+                    sessionAmt = 0;
+                } else {
+                    working = false
+                    minutes = breakTimer;
+                    seconds = 0;
+                    sessionAmt++;
+                }
+            } else if (!working) {
+                if (sessionAmt == 0) {
+                    clearInterval(timer);
+                    minutes = 0;
+                    seconds = 0;
+                } else {
+                    working = true;
+                    minutes = workTimer;
+                    seconds = 0;
+                }
+            }
         }
         if (seconds < 0) {
             seconds = 59;
@@ -85,13 +117,27 @@ resetButton.addEventListener('click', () => {
 
 workButtons.forEach(position => {
     position.addEventListener('click', function(e) {
+        workButton = true;
+        breakButton = false;
         increment(e.target.getAttribute('class'));
-        
+    });
+});
+
+brButtons.forEach(position => {
+    position.addEventListener('click', function(e) {
+        workButton = false;
+        breakButton = true;
+        increment(e.target.getAttribute('class'));
     });
 });
 
 function increment(position) {
-    let button = document.getElementsByClassName(position)[0];
+    let button;
+    if (workButton) {
+        button = document.getElementsByClassName(position)[0];
+    } else if (breakButton) {
+        button = document.getElementsByClassName(position)[1];
+    }
     if (position == 'up') {
         if (button.parentNode.id == 'work') {
             workTimer++;
@@ -103,13 +149,21 @@ function increment(position) {
                 } else {
                     minutesDisplay = minutes;
                 }
-                test.textContent = minutesDisplay + ':' + secondsDisplay;
+                if (working) {
+                    test.textContent = minutesDisplay + ':' + secondsDisplay;
+                }
             }
+        }
+
+        if (button.parentNode.id == 'break') {
+            breakTimer++;
+            brSession.textContent = breakTimer;
         }
     }
 
     if (position == 'down') {
         if (button.parentNode.id == 'work') {
+            if (workTimer == 1) {return}
             workTimer--;
             workSession.textContent = workTimer;
             if (!running) {
@@ -119,8 +173,16 @@ function increment(position) {
                 } else {
                     minutesDisplay = minutes;
                 }
-                test.textContent = minutesDisplay + ':' + secondsDisplay;
+                if (working) {
+                    test.textContent = minutesDisplay + ':' + secondsDisplay;
+                }
             }
+        }
+
+        if (button.parentNode.id == 'break') {
+            if (breakTimer == 1) {return}
+            breakTimer--;
+            brSession.textContent = breakTimer;
         }
     }
 } 
